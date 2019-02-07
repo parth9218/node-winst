a773232077b22eb0f6753b3bb87cfcc7ab7a29e8
parth <prajapatiparth06@gmail.com>
2019-02-07 14:16:22 +0530

    Fourth

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 'use strict';
 const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
 const fs = require('fs');
 const path = require('path');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const logDir = 'log';
 
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+if(!fs.existsSync(logDir))  {
+    fs.mkdirSync(logDir);
 }
 
-const filename = path.join(logDir, 'results.log');
-
-const logger = createLogger({
-  // change level if in dev environment versus production
-  level: env === 'development' ? 'debug' : 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.json()
-  ),
-  transports: [
-    new transports.Console({
-      level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-    new transports.File({ filename })
-  ]
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+    filename: `${logDir}/%DATE%-results.log`,
+    datePatter: 'YYYY-MM-DD'
 });
 
+const logger = createLogger({
+    level: env == 'development' ? 'verbose' : 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new transports.Console({
+            level: 'info',
+            format: format.combine(
+                format.colorize(),
+                format.printf(
+                    info => `${info.timestamp} ${info.level}: ${info.message}`
+                )
+            )
+        }),
+        dailyRotateFileTransport
+    ]
+});
 
-logger.info('Hello world');
+logger.debug('Debuggin info');
+logger.verbose('Verbose info');
+logger.info('Hello World');
 logger.warn('Warning message');
-logger.debug('Debugging info');
+logger.error('Error info');
+
